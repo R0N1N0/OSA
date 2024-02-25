643f613eb803a083c935cab2d14b2930f376fd75
R0N1N0 <oumirabassou@gmail.com>
2024-02-26 00:18:00 +0100

    create userAuth 2

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,22 +3,35 @@
 import db from "../db/db";
 
 // USER AUTH
-exports.getUserAuth = (req, res) => {
+exports.getUserAuth = async (req, res) => {
   const { username, password } = req.body;
+  try {
+    const results = await getUserAuthFunction(username, password);
+    if (results.length === 0) {
+      console.log("usuario no encontrado");
+    } else {
+      console.log("usuario encontrado");
+    }
+  } catch(error) {
+    console.log("error en el servidor");
+  }
+};
+
+function getUserAuthFunction(username, password) {
+  return new Promise((resolve, reject) => {
   db.query(
     `select * from usuario where username = ? and passowrd = ? `,
     [username, password],
     (error, results) => {
       if (error) {
-        console.log(`Error en la consulta ${error}`);
-      }
-      if (results.length === 0) {
+        reject(error);
       } else {
-        
+        resolve(results);
       }
     }
   );
-};
+  })
+}
 
 // INSERT NEW USER
 
@@ -28,9 +41,9 @@ exports.insertUser = (res, req) => {
     "insert into usuario (nombre, apellido, username, password, imagen, rol, puntos) values ?",
     dataUser,
     (error, result) => {
-        if(error){
-            console.log(`Error al insertar el usuario ${error}`);
-        }
+      if (error) {
+        console.log(`Error al insertar el usuario ${error}`);
+      }
     }
   );
 };
