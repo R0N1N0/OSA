cf15e114a1e278c8ec743a6ef44b74bc0cecbb33
GerardPuigOrtega <gepuor@jviladoms.cat>
2024-04-24 09:51:51 +0200

    awgeshrjdtkfy

--- Backend/app/db/db.js
+++ Backend/app/db/db.js
@@ -4,14 +4,12 @@ const mysql = require('mysql2/promise');
 
 const DB_PASSWORD = process.env.DB_PASSWORD1 + process.env.DB_PASSWORD2;
 
-async function getpool(){
-  return mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
-  });
-}
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
+});
 
-module.exports = getpool;
+module.exports = pool;
